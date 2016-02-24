aa4d392c1d01d9d9eb1f9f9705cdd4bdefe6e850
yonikan <yonikan@gmail.com>
2016-02-25 01:20:35 +0200

    gulp addon

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,51 +1,48 @@
+// GULP // --------------------------------------------------------------  //
 var gulp = require('gulp');
+
+// Concatenate & Minify JS// --------------------------------------------  //
+var concat = require('gulp-concat');
+// var uglify = require('gulp-uglify');
+// var rename = require('gulp-rename');
+gulp.task('scripts', function() {
+    return gulp.src('public/js/*.js')
+      .pipe(concat('main.js'))
+        // .pipe(rename({suffix: '.min'}))
+        // .pipe(uglify())
+        .pipe(gulp.dest('public/dist'));
+});
+
+
+// ---SASS-----------------------------------------------------------  //
 var sass = require('gulp-sass');
-// var concat = require('gulp-concat');
-
-// ----------------- Gulp CONCATINAT---------------------------------------------  //
-// gulp.task('scripts', function() {
-//     return gulp.src('public/js/*.js')
-//       .pipe(concat('ajax.js'))
-//       .pipe(gulp.dest('public/dist/'));
-// });
-
-
-// ----------------- Gulp SASS---------------------------------------------  //
-var input = './public/sass/**/*.scss';
-var output = './public/dist';
-
+var sassOptions = {
+  errLogToConsole: true,
+  outputStyle: 'expanded'
+};
 gulp.task('sass', function () {
   return gulp
-    // Find all `.scss` files from the `stylesheets/` folder
-    .src('./public/sass/**/*.scss')
-    // Run Sass on those files
-    .pipe(sass())
-    // Write the resulting CSS in the output folder
-    .pipe(gulp.dest(output));
+    .src('public/scss/**/*.scss')
+    .pipe(sass(sassOptions).on('error', sass.logError))
+    // .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('public/dist'));
 });
 
 
-// ----------------- Gulp WATCH---------------------------------------------  //
+// ---WATCH-----------------------------------------------------------  //
 gulp.task('watch', function() {
-  return gulp
-
-    //  // Watch .js files
-    //  gulp.watch('src/js/*.js', ['scripts']);
-
-    // Watch the input folder for change,
-    // and run `sass` task when something happens
-    .watch(input, ['sass'])
-    // When there is a change,
-    // log a message in the console
-    .on('change', function(event) {
-      console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
-    });
-});
+   // Watch .js files
+  gulp.watch('public/js/*.js', ['scripts']);
+   // Watch .scss files
+  gulp.watch('public/scss/*.scss', ['sass'])
+  .on('change', function(event) {
+    console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+  });
+ });
 
 
-// ----------------- Gulp RUN---------------------------------------------  //
-gulp.task('default', ['sass', 'watch']);
-
+ // Default Task // --------------------------------------------------------------  //
+gulp.task('default', ['scripts', 'sass', 'watch']);
 
 
 
@@ -59,13 +56,6 @@ gulp.task('default', ['sass', 'watch']);
 //     .pipe(gulp.dest(output));
 // });
 
-
-
-// ----------------- Gulp Conctinat---------------------------------------------  //
-// var concat = require('gulp-concat');
-//
-// gulp.task('scripts', function() {
-//   return gulp.src(['./lib/file3.js', './lib/file1.js', './lib/file2.js'])
-//     .pipe(concat({ path: 'new.js', stat: { mode: 0666 }}))
-//     .pipe(gulp.dest('./dist'));
-// });
+// - autoprefixer
+// - Generating CSS Image Sprites
+// - img optimization
