10c73653081f8910cca90942dc2ad99843f7d769
yonikan <yonikan@gmail.com>
2016-02-25 05:53:57 +0200

    fixes

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,39 +1,31 @@
+
 // GULP // --------------------------------------------------------------  //
 var gulp = require('gulp');
-
-// Concatenate & Minify JS// --------------------------------------------  //
 var concat = require('gulp-concat');
-// var uglify = require('gulp-uglify');
-// var rename = require('gulp-rename');
-gulp.task('scripts', function() {
-    return gulp.src('public/js/*.js')
-      .pipe(concat('main.js'))
-        // .pipe(rename({suffix: '.min'}))
-        // .pipe(uglify())
-        .pipe(gulp.dest('public/dist'));
-});
-
-
-// ---SASS & AutoPrifixer-----------------------------------------------------------  //
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
-// var autoprefixer = require('gulp-autoprefixer');
+
+// Concatenate JS// --------------------------------------------  //
+gulp.task('scripts', function() {
+    return gulp
+      .src('public/js/*.js')
+      .pipe(concat('main.js'))
+      .pipe(gulp.dest('public/dist'));
+});
+
+// --- SASS -----------------------------------------------------------  //
 var sassOptions = {
   errLogToConsole: true,
   outputStyle: 'expanded'
 };
-
 gulp.task('sass', function () {
   return gulp
     .src('public/scss/**/*.scss')
     .pipe(sourcemaps.init())
     .pipe(sass(sassOptions).on('error', sass.logError))
     .pipe(sourcemaps.write())
-    // .pipe(autoprefixer())
-    // .pipe(rename({suffix: '.min'}))
     .pipe(gulp.dest('public/dist'));
 });
-
 
 // ---WATCH-----------------------------------------------------------  //
 gulp.task('watch', function() {
@@ -44,8 +36,7 @@ gulp.task('watch', function() {
   .on('change', function(event) {
     console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
   });
- });
-
+});
 
  // Default Task // --------------------------------------------------------------  //
 gulp.task('default', ['scripts', 'sass', 'watch']);
@@ -53,19 +44,21 @@ gulp.task('default', ['scripts', 'sass', 'watch']);
 
 
 
-// ----------------- Gulp PRODUCTION---------------------------------------------  //
+// ---- Gulp PRODUCTION---------------------------------------------  //
 // gulp.task('prod', ['sassdoc'], function () {
 //   return gulp
-//     .src(input)
+//     .src('public/scss/**/*.scss')
 //     .pipe(sass({ outputStyle: 'compressed' }))
 //     .pipe(autoprefixer(autoprefixerOptions))
-//     .pipe(gulp.dest(output));
+//     .pipe(gulp.dest(public/dist));
 // });
 
-// - css minify
-// - autoprefixer
 // - Generating CSS Image Sprites
+// - autoprefixer
+// - minimize css
+
+// - uglify
+// - rename to .min
 // - img optimization
-// - mordinizer seperate js file on the top
-// - Regenerate SassDoc documentation
-// - Avoid any sourcemaps
+// - mordinizer seperate js file on the top (in the head tag)
+// - Avoid any sourcemaps!
--- server.js
+++ server.js
@@ -4,14 +4,13 @@ var express = require('express');
 var app = express();
 var htmlController = require('./server/htmlCtrl');
 var apiController = require('./server/apiCtrl');
+// var mongoController = require('./server/mongoCtrl');
 // var mongoUtil = require('./server/mongoCtrl');
 // mongoUtil.connect();
 
 // --------------------------------------------------------------  //
 app.set('view engine', 'ejs');
-
 app.use('/assets', express.static(__dirname + '/public'));
-
 app.use('/', function (req, res, next) {
 	console.log('Request Url:' + req.url);
 	next();
@@ -20,5 +19,7 @@ app.use('/', function (req, res, next) {
 // --------------------------------------------------------------  //
 htmlController(app);
 apiController(app);
+// mongoController(app);
+
 var port = process.env.PORT || 3000;
 app.listen(port);
